b7274e4dc27eecc144b366d72fdaa31653cf3ed7
ipshita4723 <ic4723@srmist.edu.in>
2026-09-12 11:07:36 +0530

    Add Session 5 class problem

---
+++ src/main/java/oops/class_problems/ScoreRangeAnalyzer.java
@@ -0,0 +1,16 @@
+public class ScoreRangeAnalyzer {
+    public static String findMinMaxSpread(int[] scores) {
+        if (scores == null || scores.length < 2) return "";
+        int min = scores[0];
+        int max = scores[0];
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i] < min) min = scores[i];
+            if (scores[i] > max) max = scores[i];
+        }
+        int spread = max - min;
+        return "Min: " + min + " | Max: " + max + " | Spread: " + spread;
+    }
+    public static void main(String[] args) {
+        System.out.println(findMinMaxSpread(new int[]{45, 82, 79, 90, 33, 90, 61}));
+    }
+}
