6e94c4f4372ceb234de0cfe69c9cc1d9e6a119da
ipshita4723 <ic4723@srmist.edu.in>
2026-09-12 11:05:16 +0530

    Add Session 3 class problem

---
+++ src/main/java/strings/class_problems/FileExtensionValidator.java
@@ -0,0 +1,12 @@
+public class FileExtensionValidator {
+    public static String validateFileExtension(String filename) {
+        int dotIndex = filename.lastIndexOf('.');
+        if (dotIndex == -1) return "Rejected - invalid file type";
+        String ext = filename.substring(dotIndex + 1);
+        if (ext.equalsIgnoreCase("pdf") || ext.equalsIgnoreCase("docx") || ext.equalsIgnoreCase("zip")) return "Accepted";
+        return "Rejected - invalid file type";
+    }
+    public static void main(String[] args) {
+        System.out.println(validateFileExtension("Assignment.pdf"));
+    }
+}
